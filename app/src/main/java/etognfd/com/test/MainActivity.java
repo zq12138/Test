01539f9911b21aa5c7d2be903a20a860372ff013
14537 <1453759654@qq.com>
2017-11-10 11:57:39 +0800

    test

--- app/src/main/java/etognfd/com/test/MainActivity.java
+++ app/src/main/java/etognfd/com/test/MainActivity.java
@@ -2,6 +2,7 @@ package etognfd.com.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 /**
  * test
@@ -12,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.txt);
     }
 }
